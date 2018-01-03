dacf134cd0b37f0a5309155dcf384a2913e6cabb
Ahjema <ahjema@gmail.com>
2018-01-03 15:46:19 -0500

    Update local_registry_setup.sh

--- local_registry_setup.sh
+++ local_registry_setup.sh
@@ -2,7 +2,7 @@
 
 ### First edit /etc/containers/registries.conf and add your fancy new host to the top
 ### Then generate some certs! https://docs.docker.com/registry/deploying/
-### Then run this escript
+### Then run this script
 
 docker run -d \
   --restart=always \
@@ -12,5 +12,5 @@ docker run -d \
   -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/registry.crt \
   -e REGISTRY_HTTP_TLS_KEY=/certs/registry.key \
   -p 443:443 \
-  -p 5001:443 \
+  -p 5000:443 \
   registry:2
